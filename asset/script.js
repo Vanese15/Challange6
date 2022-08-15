58fa5606c19754927d5766362ca41ca8b8d0c660
Vanese <vanesebuchanan@gmail.com>
2022-08-14 22:52:32 -0400

    Challenge6

---
+++ asset/script.js
@@ -0,0 +1,16 @@
+// Moment.js for current date
+var today = moment().format("dddd, MMMM Do YYYY, h:mm");
+console.log(today)
+
+var currentDay = document.getElementById("currentDay")
+currentDay.textContent = today
+
+apikey = bbf67ec00ac7165ca3db6258ce11872b
+
+
+
+
+    btn.addEventListener("click", function())
+
+        fetch("https://openweathermap.org/api")
+
